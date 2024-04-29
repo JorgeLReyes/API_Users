0d458e4f863df131d02b161ffd72008449529fb9
Jorge Reyes <99159473+JorgeLReyes@users.noreply.github.com>
2024-04-29 01:38:40 -0600

    Add files via upload

---
+++ src/server/index.js
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import express from "express";
+import routes from "./routes/routes.js";
+// import morgan from "morgan";
+
+const app = express();
+// app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(routes);
+
+app.listen(3000, () => console.log("http://localhost:3000"));
