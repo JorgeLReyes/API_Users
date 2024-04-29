e83e1a30a80ad0eceaa5b7dab71438ab7faf86c6
Jorge Reyes <99159473+JorgeLReyes@users.noreply.github.com>
2024-04-29 00:56:05 -0600

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,8 @@
 import "dotenv/config";
 import express from "express";
 import routes from "./routes/routes.js";
-import morgan from "morgan";
 
 const app = express();
-app.use(morgan("dev"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
