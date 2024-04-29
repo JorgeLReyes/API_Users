0597cbcb3d9a1d3956e1e31bcc364e23769e3517
Jorge Reyes <99159473+JorgeLReyes@users.noreply.github.com>
2024-04-29 11:33:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import { createClient } from "@supabase/supabase-js";
+import cors from "cors";
 
 const app = express();
 
@@ -8,6 +9,7 @@ const supabaseUrl = process.env.JS_SUPABASE_URL;
 const supabaseKey = process.env.JS_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
