b7da3a26e2d1810ddd98d9953534b4a97495fd24
Jorge Reyes <99159473+JorgeLReyes@users.noreply.github.com>
2024-04-29 01:04:36 -0600

    Delete src/server directory

--- src/server/config/supabaseClient.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.JS_SUPABASE_URL;
-const supabaseKey = process.env.JS_SUPABASE_KEY;
-export const supabase = createClient(supabaseUrl, supabaseKey);
